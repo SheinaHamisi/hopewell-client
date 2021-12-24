74b1ef01cadbf2159fbb4d7a52f3edd405d44b86
linco6054 <lincoln.kantet@gmail.com>
2021-12-24 21:23:03 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './assets/images/logo.png';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.png";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App-header">
+    <div className="App-header dark">
       <img src={logo} className="App-logo" alt="logo" />
+
+      <h2 className="text-primary">
+        This is the primary colors corporate colors
+      </h2>
     </div>
   );
 }

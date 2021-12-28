b416332da6ddc37a8a6f4145f009707d2e26aed8
SheinaHamisi <hamisisheina@gmail.com>
2021-12-28 15:42:25 +0300

    added activate page

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { toast } from "react-toastify";
+// import Input from "./components/Input";
+// import PrimaryButton from "./components/Button";
 
 function Login() {
   return <div>login</div>;

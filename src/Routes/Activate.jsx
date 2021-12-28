ac02a63039e1cd945f6569ea669e2b94a80c10cf
ericwafula <wathomeeric@gmail.com>
2021-12-28 18:37:48 +0300

    added activate page content

--- src/Routes/Activate.jsx
+++ src/Routes/Activate.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ActivateButton from './components/ActivateButton'
+import Illustration from './../assets/images/illustration.png'
 
 function Activate() {
     return (
-        <div>
-            Activate page
+        <div className='bg-primary w-full h-screen sm:p-6 md:p-14 grid items-center'>
+            <div className="bg-white grid md:grid-cols-1 xl:grid-cols-3 rounded sm:p-6 md:p-14 h-full">
+                <div className='col-span-2 md:grid items-center hidden'>
+                    <img src={Illustration} alt="happy" />
+                </div>
+                <div className='col-span-1 flex items-center justify-center flex-col px-5'>
+                    <p className='text-gray-700 font-bold text-7xl text-center'>Hooray Eric!</p>
+                    <p className='mt-8 text-xl text-center mb-8'>You're almost there! Just one click away to activate your HopeWell Account.</p>
+                    <ActivateButton text="Activate"/>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/Routes/components/ActivateButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ActivateButton({text}) {
+    return (
+        <button className="bg-primary w-full h-16 rounded text-xl text-white font-semibold">{text}</button>
+    )
+}
+
+export default ActivateButton

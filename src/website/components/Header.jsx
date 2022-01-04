cacf2c6368d03f8cc64da539e29bd3cbeb7ccf9b
SheinaHamisi <hamisisheina@gmail.com>
2022-01-04 16:52:25 +0300

    landing page

--- src/website/components/Header.jsx
+++ src/website/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Header() {
-  return <div>header</div>;
+  return <div><div className="header flex md:justify-between">
+  <div>
+    <img
+      src="logo.png"
+      className="w-16 md:w-28 lg:w-36 md:ml-8 lg:ml-20"
+      alt=""
+    />
+  </div>
+
+  <div className="flex items-center md:space-x-8 md:mr-12 lg:text-xl text-xs">
+    <p>HOME</p>
+    <p>HOW IT WORKS</p>
+    <p>FAQS</p>
+    <p>PRICING</p>
+    <p>CONTRACT</p>
+  </div>
+</div></div>;
 }
 
 export default Header;

920ec46f1f56d705cad5000f4e89912ebe17e3fd
ericwafula <wathomeeric@gmail.com>
2022-01-11 10:43:31 +0300

    added dash layout

--- src/features/api/index.js
+++ src/features/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import decode from "jwt-decode";
 
-export const Server_URL = "http://localhost:5000"; //"https://hopewell-firm.herokuapp.com"; //  "http://localhost:5000";
+export const Server_URL = /**"http://localhost:5000"; **/ "https://hopewell-firm.herokuapp.com"; //  "http://localhost:5000";
 const API_URL = `${Server_URL}/api`;
 
 export const API = axios.create({
